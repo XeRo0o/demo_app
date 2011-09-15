51f23a86ed60a255dcfc0f27f6677d99e64b0b5f
William John McCartin III <admin@myxerotek.com>
2011-09-15 19:28:10 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '4f68023ec9f66d6429d4872fd460fd61a678a47500083e0e874c4be9901a2cfbe83dad890a4c4c0f8b3117bbaa03bacbb7638af95685cc4586faf196da83d013'
